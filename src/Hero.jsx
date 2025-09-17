2dc6762eefb1a59dea560e66379a5dbedc36bb35
Chantelle Lavery <chantelle.hume@hotmail.com>
2025-09-17 11:32:02 +0100

    added comments

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,6 +7,7 @@ function Hero() {
     // Here im using a boolean state to toggle the menu open and closed. Im setting 'menuOpen' to false as default, and setMenuOpen is the fucntion to change the state to True.
   const [menuOpen, setMenuOpen] = useState(false)
 
+  // Togglemenu will be called on click, and will set the menuOpen state to the opposite of what it currently is. This means I dont need a separate function to close the menu.
   const toggleMenu = () => setMenuOpen(prev => !prev)
 
   return (
